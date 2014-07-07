0b64d2054856f830d322ce928562befac9f65809
oleg.ivanov.nabusteam <oleg.ivanov.nabusteam@gmail.com>
2014-07-08 02:27:19 +0300

    Session Storage

---
+++ CI/CI.Repository/SessionStorage/PersistenceContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace CI.Repository.SessionStorage
+{
+    public class PersistenceContext
+    {
+        private static Configuration _nhibernateConfiguration;
+
+        private static ISessionFactory _sessionFactory;
+
+        public static void Initialize(string connectionString)
+        {
+            var databaseConfiguration = MsSqlConfiguration.MsSql2012.ConnectionString(connectionString);
+
+            _nhibernateConfiguration = Fluently.Configure()
+                .Database(databaseConfiguration)
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<PersistenceContext>())
+                .BuildConfiguration();
+
+            // tell NHibernate to quote columns with reserved names like "Order"
+            // note it affects all queries, not only a schema updater
+            SchemaMetadataUpdater.QuoteTableAndColumns(_nhibernateConfiguration);
+
+            _sessionFactory = _nhibernateConfiguration.BuildSessionFactory();
+        }
+
+        public static ISession GetCurrentSession()
+        {
+            ISessionStorageContainer sessionStorageContainer = SessionStorageFactory.GetStorageContainer();
+
+            ISession currentSession = sessionStorageContainer.GetCurrentSession();
+
+            if (currentSession == null)
+            {
+                currentSession = GetNewSession();
+                sessionStorageContainer.Store(currentSession);
+            }
+
+            return currentSession;
+        }
+
+        private static ISession GetNewSession()
+        {
+            return _sessionFactory.OpenSession();
+        }
+    }
+}
---
+++ CI/CI.Repository/SessionStorage/SessionStorageFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace CI.Repository.SessionStorage
+{
+    public static class SessionStorageFactory
+    {
+        private static ISessionStorageContainer _nhSessionStorageContainer;
+
+        public static ISessionStorageContainer GetStorageContainer()
+        {
+            if (_nhSessionStorageContainer == null)
+            {
+                if (HttpContext.Current == null)
+                    _nhSessionStorageContainer = new ThreadSessionStorageContainer();
+                else
+                    _nhSessionStorageContainer = new HttpSessionStorageContainer();
+            }
+
+            return _nhSessionStorageContainer;
+        }
+    }
+}
